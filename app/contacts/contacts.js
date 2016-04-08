f5169b18f176139d5a143ac74413d04e96e09342
Maksim Murygin <muriginm@gmail.com>
2016-04-09 00:29:13 +0600

    add forms

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -5,10 +5,20 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/contacts', {
     templateUrl: 'contacts/contacts.html',
-    controller: 'ContactCtrl'
+    controller: 'ContactsCtrl'
   });
 }])
 
-.controller('ContactCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-  console.log($scope);
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var db = new Firebase('https://ngloginapp.firebaseio.com');
+
+  $scope.contacts = $firebaseArray(db);
+  
+  $scope.showAddForm = function () {
+    $scope.addFormShow = true; 
+  };
+
+  $scope.addFormSubmit = function () {
+    $scope.addFormShow = false;
+  };
 }]);
