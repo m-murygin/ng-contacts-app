f51a84d4b853126f6852fc052753f19de2b879a3
Maksim Murygin <muriginm@gmail.com>
2016-04-09 10:24:18 +0600

    implementing editing contacts

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -19,6 +19,65 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
   };
 
   $scope.addFormSubmit = function () {
-    $scope.addFormShow = false;
+    $scope.contacts
+      .$add({
+        name: $scope.name,
+        email: $scope.email || null,
+        phone: $scope.phone || null,
+        company: $scope.company || null
+      })
+      .then(function (ref) {
+        var id = ref.key();
+        console.log('Added contact with id %s', id);
+
+        $scope.addFormShow = false;
+        $scope.msg = 'Contact with ' + $scope.name + ' was added';
+
+        clearFormFields();
+      });
   };
+
+  $scope.showContact = function (contact) {
+    $scope.currentContact = contact;
+    $scope.contactFormShow = true;
+  };
+
+  $scope.hideContactForm = function () {
+    $scope.contactFormShow = false;
+  };
+
+  $scope.showEditForm = function (contact) {
+    $scope.id = contact.$id;
+    $scope.name = contact.name;
+    $scope.email = contact.email;
+    $scope.phone = contact.phone;
+    $scope.company = contact.company;
+    $scope.editFormShow = true;
+  };
+
+  $scope.editFormSubmit = function () {
+    var id = $scope.id;
+
+    var record = $scope.contacts.$getRecord(id);
+
+    record.name = $scope.name;
+    record.email = $scope.email;
+    record.phone = $scope.phone;
+    record.company = $scope.company;
+
+    $scope.contacts
+      .$save(record)
+      .then(function (ref) {
+        console.log('Was successfully edited %s', ref.key());
+        $scope.editFormShow = false;
+        clearFormFields();
+      });
+  };
+
+  function clearFormFields() {
+    $scope.name = null;
+    $scope.email = null;
+    $scope.phone = null;
+    $scope.company = null;
+  }
 }]);
